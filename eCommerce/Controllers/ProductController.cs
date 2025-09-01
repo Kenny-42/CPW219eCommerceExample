ffe6193bd337946e1019938145d45d9660627749
Kendall Dawson <lilkenny642@gmail.com>
2025-08-31 20:13:21 -0700

    Refactor ProductController methods and add DeleteConfirmed
    Updated the `Edit` and `Delete` methods to streamline product retrieval by combining `Where` and `FirstOrDefault` into a single line. Added a new `DeleteConfirmed` method to handle HTTP POST requests for product deletion, including checks for existence, removal from the context, asynchronous saving, and success messaging.

--- eCommerce/Controllers/ProductController.cs
+++ eCommerce/Controllers/ProductController.cs
@@ -46,8 +46,7 @@ public class ProductController : Controller
     public IActionResult Edit(int id)
     {
         Product? product = _context.Products
-            .Where(p => p.ProductId == id)
-            .FirstOrDefault();
+            .Where(p => p.ProductId == id).FirstOrDefault();
 
         if (product == null)
         {
@@ -75,8 +74,7 @@ public class ProductController : Controller
     public IActionResult Delete(int id)
     {
         Product? product = _context.Products
-            .Where(p => p.ProductId == id)
-            .FirstOrDefault();
+            .Where(p => p.ProductId == id).FirstOrDefault();
 
         if (product == null)
         {
@@ -84,5 +82,23 @@ public class ProductController : Controller
         }
 
         return View(product);
+    }
+
+    [ActionName(nameof(Delete))]
+    [HttpPost]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        Product? product = await _context.Products.FindAsync(id);
+
+        if (product == null)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        _context.Remove(product);
+        await _context.SaveChangesAsync();
+
+        TempData["Message"] = $"{product.Title} was deleted successfully!";
+        return RedirectToAction(nameof(Index));
     }
 }
