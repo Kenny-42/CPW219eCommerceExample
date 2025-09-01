4e064e2d6169a1a3560963f3357a1a35dcba915f
Kendall Dawson <lilkenny642@gmail.com>
2025-08-31 20:39:06 -0700

    Add Member class to eCommerce.Models namespace
    Introduced a new `Member` class representing individual website users.
    The class includes properties for `MemberId`, `Username`, `Email`, `Password`,
    and `DateOfBirth`, with `Username`, `Email`, and `Password` marked as required.
    XML documentation comments were added for clarity on the class and its properties.

---
+++ eCommerce/Models/Member.cs
@@ -0,0 +1,35 @@
+﻿namespace eCommerce.Models;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Represents an individual website user
+/// </summary>
+public class Member
+{
+    /// <summary>
+    /// Unique identifier for the member
+    /// </summary>
+    [Key]
+    public int MemberId { get; set; }
+
+    /// <summary>
+    /// Public facing username for the member.
+    /// Alphanumberic characters only
+    /// </summary>
+    public required string Username { get; set; }
+
+    /// <summary>
+    /// Email for the member
+    /// </summary>
+    public required string Email { get; set; }
+
+    /// <summary>
+    /// The member's password
+    /// </summary>
+    public required string Password { get; set; }
+
+    /// <summary>
+    /// Date of birth for the member
+    /// </summary>
+    public DateOnly DateOfBirth { get; set; }
+}
